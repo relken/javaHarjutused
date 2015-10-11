5111e723ba2e37151b3beaff84ac2cea8ca05fbd
Rain Elken <relken@klass1-13-l.intra.itcollege.ee>
2015-10-11 10:28:29 +0300

    Praktikum2

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -10,10 +10,68 @@ import lib.Foor;
  */
 public class Peamurdja2_ristmik extends Application {
     @Override
+
+
     public void start(Stage primaryStage) throws Exception {
-        Foor foor1 = new Foor(Foor.YLEMINE):
-        Foor foor2 = new Foor(Foor.ALUMINE)
-        Foor foor3 = new Foor(Foor.PAREM)
-        Foor foor4 = new Foor(foor.YLEMINE)
+        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foor3 = new Foor(Foor.PAREM, primaryStage);
+        Foor foor4 = new Foor(Foor.VASAK, primaryStage);
+
+
+        int count = 0;
+        while (count < 10) {
+            vertikaal(foor1, foor2);
+            horisontaal(foor3, foor4);
+        count++;
+        }
     }
+
+    public void vertikaal(Foor foor1, Foor foor2){
+        foor1.vahetaRohelist();
+        foor2.vahetaRohelist();
+        rohelisestPunaseks(foor1);
+        rohelisestPunaseks(foor2);
+        punasestRoheliseks(foor1);
+        punasestRoheliseks(foor2);
+
+    }
+    public void horisontaal(Foor foor3, Foor foor4){
+        foor3.vahetaPunast();
+        foor4.vahetaPunast();
+        punasestRoheliseks(foor3);
+        punasestRoheliseks(foor4);
+        rohelisestPunaseks(foor3);
+        rohelisestPunaseks(foor4);
+    }
+
+
+    public void rohelisestPunaseks(Foor foor) {
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+        //foor.vahetaPunast();
+        //foor.paus(3);
+
+    }
+   public void punasestRoheliseks(Foor foor){
+       foor.vahetaPunast();
+       foor.paus(3);
+       foor.vahetaKollast();
+       foor.paus(0.5);
+       foor.vahetaKollast();
+       foor.vahetaPunast();
+    }
+
 }
